5760f50732ce61eecb4075510a7c1ded5faa0168
Yuri Sanchez <yuri.tecnologia@outlook.com>
2026-04-23 17:23:20 -0300

    feat: Implementa mappers e DTO para a entidade usuarios

--- src/main/java/com/sanchez/cruddeusuarios/Usuarios/UsuariosDTO.java
+++ src/main/java/com/sanchez/cruddeusuarios/Usuarios/UsuariosDTO.java
@@ -1,4 +1,17 @@
 package com.sanchez.cruddeusuarios.Usuarios;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class UsuariosDTO {
+    private Long id;
+    private String nome;
+    private int idade;
+    private String email;
+    private String cpf;
 }
--- src/main/java/com/sanchez/cruddeusuarios/Usuarios/UsuariosMapper.java
+++ src/main/java/com/sanchez/cruddeusuarios/Usuarios/UsuariosMapper.java
@@ -1,4 +1,29 @@
 package com.sanchez.cruddeusuarios.Usuarios;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class UsuariosMapper {
+
+    public UsuariosModel map(UsuariosDTO usuariosDTO){
+        UsuariosModel usuariosModel = new UsuariosModel();
+        usuariosModel.setId(usuariosDTO.getId());
+        usuariosModel.setNome(usuariosDTO.getNome());
+        usuariosModel.setIdade(usuariosDTO.getIdade());
+        usuariosModel.setEmail(usuariosDTO.getEmail());
+        usuariosModel.setCpf(usuariosDTO.getCpf());
+
+        return usuariosModel;
+    }
+
+    public UsuariosDTO map(UsuariosModel usuariosModel){
+        UsuariosDTO usuariosDTO = new UsuariosDTO();
+        usuariosDTO.setId(usuariosModel.getId());
+        usuariosDTO.setNome(usuariosModel.getNome());
+        usuariosDTO.setIdade(usuariosModel.getIdade());
+        usuariosDTO.setEmail(usuariosModel.getEmail());
+        usuariosDTO.setCpf(usuariosModel.getCpf());
+
+        return usuariosDTO;
+    }
 }
